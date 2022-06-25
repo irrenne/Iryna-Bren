39e7164a3f9e46cbfd728e2a159c007c7043f9e3
irrenne <breniryna@gmail.com>
2022-06-25 21:46:03 +0300

    homework1 done

---
+++ src/main/java/com/epam/spring/homework1/pet/Spider.java
@@ -0,0 +1,10 @@
+package com.epam.spring.homework1.pet;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Spider implements Animal{
+    public String getAnimal(){
+        return "Spider";
+    }
+}
